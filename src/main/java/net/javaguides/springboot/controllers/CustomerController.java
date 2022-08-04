6c2692bba1d232f2d63a74eba588b24b86e0461f
mb-charlie <92309564+mb-charlie@users.noreply.github.com>
2022-08-05 00:41:14 +0530

    successfully sending excel from postman

--- src/main/java/net/javaguides/springboot/controllers/CustomerController.java
+++ src/main/java/net/javaguides/springboot/controllers/CustomerController.java
@@ -2,13 +2,10 @@ package net.javaguides.springboot.controllers;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,29 +22,15 @@ import net.javaguides.springboot.services.CustomerServices;
 @CrossOrigin("*")
 @JsonIgnoreType()
 public class CustomerController {
-	private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
 	@Autowired
 	CustomerServices customerServices;
 	@Autowired
 	private CustomerRepository customerRepository;
+	
 	@GetMapping("/custlist")
 	public List<CustomerModel> getCustList(){
 		return customerServices.getAllCustomers();
 	}
-	
-//	@PostMapping("/addcustlist")
-//	public List<CustomerModel> addCustList(@RequestBody List<CustomerModel> customerModel) {
-////		for(CustomerModel C:customerModel) {
-////			log.info("Adding record :"+C);
-////			customerRepository.save(C);
-////		}
-//		return customerModel;
-//	}
-	
-//	@PostMapping("/addcustlist")
-//    public List<CustomerModel> addCustList(@RequestBody List<CustomerModel> p) {
-//        return (List<CustomerModel>)customerRepository.saveAll(p);
-//    }
 	
 	@PostMapping("/upload")
 	public void upload(@RequestParam("file") MultipartFile file) {
--- src/main/java/net/javaguides/springboot/services/CustomerServices.java
+++ src/main/java/net/javaguides/springboot/services/CustomerServices.java
@@ -5,6 +5,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.NumberToTextConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,10 +13,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import net.javaguides.springboot.models.CustomerModel;
 import net.javaguides.springboot.repositories.CustomerRepository;
@@ -25,7 +31,9 @@ public class CustomerServices {
 
 	@Autowired
 	private CustomerRepository customerRepository;
-	
+
+    private ObjectMapper mapper = new ObjectMapper();
+    
 //	To save all customers
 	public String setAllCustomers(CustomerModel custms) {
 		customerRepository.save(custms);
@@ -42,15 +50,54 @@ public class CustomerServices {
 		file.transferTo(tempFile);
 		Workbook workbook = WorkbookFactory.create(tempFile );
 		Sheet sheet = workbook.getSheetAt(0);
-		Stream<Row> rowStream = StreamSupport.stream (sheet.spliterator(), false) ;
-		rowStream.forEach(row -> {
-			Stream<Cell> cellStream = StreamSupport.stream(row.spliterator(), false);
-			List<Object> cellVals= cellStream.map(cell -> {
-				String cellVal = cell.getStringCellValue();
-				return cellVal;
-			})
-			.collect(Collectors.toList());
-			System.out.println(cellVals);
-		});
+//		Stream<Row> rowStream = StreamSupport.stream (sheet.spliterator(), false) ;
+//		rowStream.forEach(row -> {
+//			Stream<Cell> cellStream = StreamSupport.stream(row.spliterator(), false);
+//			List<Object> cellVals= cellStream.map(cell -> {
+//				String cellVal = cell.getStringCellValue();
+//				return cellVal;
+//			})
+//			.collect(Collectors.toList());
+//			System.out.println(cellVals);
+//		});
+		List<String> headers = new ArrayList<String>();
+        ArrayNode sheetData = mapper.createArrayNode();
+        for (int j = 0; j <= sheet.getLastRowNum(); j++) {
+        	Row row = sheet.getRow(j);
+        	if (j == 0) {
+                // reading sheet header's name
+                for (int k = 0; k < row.getLastCellNum(); k++) {
+                    headers.add(row.getCell(k).getStringCellValue());
+                }
+            } else {
+                // reading work sheet data
+                ObjectNode rowData = mapper.createObjectNode();
+                for (int k = 0; k < headers.size(); k++) {
+                    Cell cell = row.getCell(k);
+                    String headerName = headers.get(k);
+                    if (cell != null) {
+                        switch (cell.getCellType()) {
+                        case BOOLEAN:
+                            rowData.put(headerName, cell.getBooleanCellValue());
+                            break;
+                        case NUMERIC:
+                        	String str = NumberToTextConverter.toText(cell.getNumericCellValue());
+                        	long l=Long.parseLong(str);
+                            rowData.put(headerName, l);
+                            break;
+                        case STRING:
+                            rowData.put(headerName, cell.getStringCellValue());
+                            break;
+						default:
+							break;
+                        }
+                    }
+                }
+                if(!rowData.toString().equals("{}")) {
+                    sheetData.add(rowData);
+                }
+            }
+        }
+        System.out.println(sheetData);
 	}
 }
